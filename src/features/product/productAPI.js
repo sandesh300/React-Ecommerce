ce954773743abebffbbea7782eb5432794af3f13
sandesh <bhujbalsandesh52@gmail.com>
2023-07-30 18:58:26 +0530

    Part : 7 Filter and Sort changes accurately

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,26 +1,37 @@
 export function fetchAllProducts() {
-  return new Promise(async(resolve) => {
-    //TODO : we will not hardcode server url here 
-    const response = await fetch('http://localhost:8080/products')
-    const data = await response.json();
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
     resolve({data})
-}
-    );
-}
-
-
-export function fetchProductsByFilters(filter) {
-  //filter  = {"category" : "smartphone"}
-  let queryString ='';
-  for(let key in filter)
-  {
-    queryString += `${key}=${filter[key]}&`
   }
-  return new Promise(async(resolve) => {
-    //TODO : we will not hardcode server url here 
-    const response = await fetch('http://localhost:8080/products?'+queryString)
-    const data = await response.json();
-    resolve({data})
+  );
 }
-    );
+
+export function fetchProductsByFilters(filter,sort) {
+  // filter = {"category":["smartphone","laptops"]}
+  // sort = {_sort:"price",_order="desc"}
+
+  // TODO : on server we will support multi values in filter
+  let queryString = '';
+  for(let key in filter){
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+  }
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+
+
+
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
 }
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { 
-  fetchAllProducts, 
-  fetchProductsByFilters, 
-   } from './productAPI';
+import { fetchAllProducts,fetchProductsByFilters } from './productAPI';
 
 const initialState = {
   products: [],
   status: 'idle',
 };
+
 
 export const fetchAllProductsAsync = createAsyncThunk(
   'product/fetchAllProducts',
@@ -19,18 +17,21 @@ export const fetchAllProductsAsync = createAsyncThunk(
 );
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
-  async (filter) => {
-    const response = await fetchProductsByFilters(filter);
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilters(filter,sort);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
+
+
 
 export const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
     increment: (state) => {
+      state.value += 1;
     },
   },
   extraReducers: (builder) => {
@@ -42,12 +43,12 @@ export const productSlice = createSlice({
         state.status = 'idle';
         state.products = action.payload;
       })
-        .addCase(fetchProductsByFiltersAsync.pending, (state) => {
-          state.status = 'loading';
-        })
-        .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
-          state.status = 'idle';
-          state.products = action.payload;
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
       });
   },
 });
