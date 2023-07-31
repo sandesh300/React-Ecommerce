7d8baf624ab0fe04a8ba422bb9f11dec5832cdab
sandesh <bhujbalsandesh52@gmail.com>
2023-07-31 19:19:56 +0530

    Part : 12 cart functionalities update and remove

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from '../features/product/productSlice';
 import authReducer from '../features/auth/authSlice';
+import cartReducer from '../features/cart/cartSlice';
 export const store = configureStore({
   reducer: {
     product: productReducer,
-    auth: authReducer
+    auth: authReducer,
+    cart: cartReducer
   },
 });
