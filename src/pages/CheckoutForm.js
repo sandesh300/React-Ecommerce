81dbf49e1b504e1653e0846ebd75a3b87282971b
sandesh <bhujbalsandesh52@gmail.com>
2023-08-14 17:52:32 +0530

    Part 26.1: Setting Proxy and Admin Panel order payment status

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `localhost:3000/order-success/${currentOrder.id}`,
+        return_url: `https://mern-ecommerce-liard.vercel.app/login/order-success/${currentOrder.id}`,
       },
     });
 
--- src/pages/StripeCheckout.js
+++ src/pages/StripeCheckout.js
@@ -13,7 +13,6 @@ import { selectCurrentOrder } from "../features/order/orderSlice";
 //const stripePromise = loadStripe("");
 const stripePromise = loadStripe("pk_test_51NdpqJSHE2TubihN6uuMHt8Ji1vxc6DfhN70dmV3ZfUBYML7SJY0tBRUjIpgLyp3JlyOluCML03xhpxzHDQEU7AM00ekdhyBzh");
 
-
 export default function StripeCheckout() {
   const [clientSecret, setClientSecret] = useState("");
   const currentOrder = useSelector(selectCurrentOrder)
@@ -23,12 +22,10 @@ export default function StripeCheckout() {
     fetch("/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ totalAmount: currentOrder.totalAmount }),
-      meta:{
-        order_id: currentOrder.id 
+      body: JSON.stringify({ totalAmount: currentOrder.totalAmount, orderId: currentOrder.id }),
         // this info will go to stripe => and then to our webhook
         // so we can conclude that payment was successful, even if client closes window after pay
-      }
+      
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
